fbb6af22305762fa1816f1b0ccd17c2c9a056b9f
Marc Roussel <marc.roussel04@outlook.fr>
2025-06-08 14:55:27 +0200

    Importing Header in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
+import Header from "../components/Header/Header";
+
 const HomePage = () => {
     return (
-
+        <>
+            <Header />
+            <p>This is the HomePage.</p>
+        </>
     );
 }
 
