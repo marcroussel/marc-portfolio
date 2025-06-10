f69879c851a5751df372ceddaf9f2c6c0e03c2b3
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2025-06-10 21:58:21 +0200

    Finishing first header design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,13 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
-        <span className={styles.header}>
+        <div className={styles.header}>
             <img alt="Photo de Profil" className={styles.profilePicture} src="src/assets/photo_profil.jpg" />
             <div className={styles.nameSection}>
                 <p className={styles.name}>Marc Roussel</p>
                 <p className={styles.subtitle}>Etudiant en 3ème année de BUT Informatique</p>
             </div>
-        </span>
+        </div>
     );
 }
 
