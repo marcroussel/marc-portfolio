505d9bb47532a7dbecb506bf9515bc286a112609
Marc Roussel <marc.roussel04@outlook.fr>
2025-06-11 14:09:04 +0200

    Setting colors for backgrounds and useless line

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,19 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <img alt="Photo de Profil" className={styles.profilePicture} src="src/assets/photo_profil.jpg" />
-            <div className={styles.nameSection}>
-                <p className={styles.name}>Marc Roussel</p>
-                <p className={styles.subtitle}>Etudiant en 3ème année de BUT Informatique</p>
+        <>
+            <div className={styles.header}>
+                <img alt="Photo de Profil" className={styles.profilePicture} src="src/assets/photo_profil.jpg" />
+                <div className={styles.nameSection}>
+                    <p className={styles.name}>Marc Roussel</p>
+                    <p className={styles.subtitle}>Etudiant en 3ème année de BUT Informatique</p>
+                </div>
             </div>
-        </div>
+            <div className={styles.linebar}/>
+            <div className={styles.navbar}>
+                <p>My Pages incoming</p>
+            </div>
+        </>
     );
 }
 
